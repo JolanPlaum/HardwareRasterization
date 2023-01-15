8ce23e29dc8a8bb4b515caf48687932826d907fa
Jolan Plaum <jolan.plaum@student.howest.be>
2023-01-15 12:13:48 +0100

    Created Effect class

---
+++ source/Effect.h
@@ -0,0 +1,42 @@
+#pragma once
+// Includes
+
+namespace dae
+{
+	// Class Declaration
+	class Effect final
+	{
+	public:
+		// Constructors and Destructor
+		Effect(ID3D11Device* pDevice, const std::wstring& assetFile);
+		~Effect();
+
+		// Copy and Move semantics
+		Effect(const Effect& other)					= delete;
+		Effect& operator=(const Effect& other)		= delete;
+		Effect(Effect&& other) noexcept				= delete;
+		Effect& operator=(Effect&& other) noexcept	= delete;
+
+		//---------------------------
+		// Public Member Functions
+		//---------------------------
+		static ID3DX11Effect* LoadEffect(ID3D11Device* pDevice, const std::wstring& assetFile);
+
+		ID3DX11Effect* GetEffect() { return m_pEffect; }
+		ID3DX11EffectTechnique* GetTechnique() { return m_pTechnique; }
+		ID3D11InputLayout* GetInputLayout() { return m_pInputLayout; }
+
+
+	private:
+		// Member variables
+		ID3DX11Effect* m_pEffect{};
+		ID3DX11EffectTechnique* m_pTechnique{};
+
+		ID3D11InputLayout* m_pInputLayout{};
+
+		//---------------------------
+		// Private Member Functions
+		//---------------------------
+
+	};
+}
